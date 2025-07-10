d9979690f58b74c0cf3c530759c7608bb92b2076
Brandon Trieu <brandtrieu@gmail.com>
2025-07-10 02:59:12 -0400

    added basic edit and submit functionality

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,75 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 
 
-function HeaderForm() {
+function EditorAndDisplay() {
+  const [headerData, setHeaderData] = useState({name: '', email: '', phone: ''});
+  const [submitData, setSubmitData] = useState({name: '', email: '', phone: ''});
+  const [editMode, setEditMode] = useState(true);
+  
+  const updateHeaderData = (e) => {
+    // take the name and value from the event target, which will be the input that triggered the change
+    const { name, value } = e.target;
+    // update the headerData state with the new value
+    setHeaderData(prev => ({ ...prev, [name]: value }));
+  };
+
+  const updateSubmittedData = () =>{
+    setSubmitData(headerData);
+    setEditMode(false);
+  };
+
+  const enableEditMode = () => {
+    setEditMode(true);
+  }
+
+
+  return ( 
+    <>
+      {editMode ? <HeaderForm data={headerData} changeData={updateHeaderData} /> : null}
+      <DisplayResume data={submitData} changeData={updateSubmittedData} editState={editMode} changeEditState={enableEditMode}/>
+    </>
+  )
+}
+
+
+function HeaderForm({data, changeData}) {
   return (
     <form>
       <label for="name">Name</label>
-      <input type="name" id="name" name="name" required />
+      <input 
+        type="text" 
+        name="name" 
+        value={data.name} 
+        onChange={changeData} />
     
       <label for="email">Email</label>
-      <input type="email" id="email" name="email" required />
+      <input 
+        type="email" 
+        name="email" 
+        value={data.email} 
+        onChange={changeData} />
 
       <label for="phone">Phone</label>
-      <input type="number" id="phone" name="phone" required />
-      
-    
+      <input 
+        type="number" 
+        name="phone" 
+        value={data.phone} 
+        onChange={changeData} />
     </form>
   )
 }
 
-export default HeaderForm
+function DisplayResume({data, changeData, editState, changeEditState}) {
+  return (
+    <div>
+      <p>Name: {data.name}</p>
+      <p>Email: {data.email}</p>
+      <p>Phone: {data.phone}</p>
+      <button onClick={changeData}>Submit</button>
+      {editState ? null : <button onClick={changeEditState}>Edit</button>}
+    </div>
+  )
+}
+
+
+export { HeaderForm, DisplayResume, EditorAndDisplay }
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import HeaderForm from './components/header.jsx'
+import { HeaderForm, DisplayResume, EditorAndDisplay } from './components/header.jsx'
 import ExperienceForm from './components/experience.jsx'
 import EducationForm from './components/education.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <h1 className="heading">Resume Builder</h1>
-    <HeaderForm />
-    <ExperienceForm />
-    <EducationForm />
+    <EditorAndDisplay />
+    
   </StrictMode>,
 )
